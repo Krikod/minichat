57b149e6a015188ab59e642ac6920700c39179b6
kp <kristen.kod@gmail.com>
2017-04-09 21:54:49 +0200

    Prepare files

---
+++ minichat.php
@@ -0,0 +1,2 @@
+<?php 
+// contient le formulaire permettant d'ajouter un message et liste les 10 derniers messages.
---
+++ minichat_post.php
@@ -0,0 +1,11 @@
+<?php
+// insère le message reçu avec $_POST dans la base de données puis redirige vers minichat.php.
+
+// Effectuer ici la requête qui insère le message
+
+
+
+// Puis rediriger vers minichat.php
+header('location: minichat.php');
+
+?>
